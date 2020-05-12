71918f896965eb68c5a68809cc2882dc4b2120ea
simonwa7 <simonwa7@gmail.com>
2020-05-11 20:14:50 -0400

    adding comments

--- server.js
+++ server.js
@@ -5,45 +5,49 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
-// mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
 
+// Setup Ssrver packages/configuration
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(bodyParser.json())
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+
+
+// Creation of object connection for MongoDB User collection 
 const User = mongoose.model('User', {
   name: String,
   email: String,
 })
 
-app.use(cors());
-app.use(express.static(path.join(__dirname, 'build')));
-app.use(bodyParser.json())
 
+// Server method to handle user login 
 app.post('/login/user', function (req, res) {
   console.log('in server.js /login/user')
 
-  User.findOne({email: req.body.email})
-  .then(result => {
-    if(!result) {
-      new User(req.body)
-        .save()
-        .then((result) => {
-          res.status(201).end()
-        })
-        .catch((err) => {
-          console.log('did not work!!')
-          res.status(500).end(err.message)
-        })
-    }
+  User.findOne({ email: req.body.email }).then(result => {
 
-    else {
+    // If there does not exist a user in the database with the same email, create a new one
+    console.log(result)
+    if (!result) {
+
+      new User(req.body).save().then((result) => {
+        // Return a 201 indicating a new user was created
+        console.log("Yaaaaaay new friend with new plantlings")
+        res.status(201).json({ "message": "welcome! we have new user :D" })
+      }).catch((err) => {
+        // Return a 500 if there was a problem inserting into the database
+        console.log('did not work!! whooopsies')
+        res.status(500).json({ "error": err.message })
+      })
+    } else {
+      // User exists, so we return a 200 indicating the user successfully signed in
       console.log('already have dem')
-      res.status(200).json({error: "hello! you already exist!"})
+      res.status(200).json({ "message": "hello! you already exist!" })
     }
-
   })
-
 
 });
 
-app.listen(process.env.PORT || 3001, function(){
+app.listen(process.env.PORT || 3001, function () {
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
