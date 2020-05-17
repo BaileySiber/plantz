88140ae8b1174f918d97072079b183bf1929d4df
Bailey <bailey.siber@tufts.edu>
2020-05-16 21:50:45 -0400

    added add and delete plant routes

--- server.js
+++ server.js
@@ -19,6 +19,18 @@ const User = mongoose.model('User', {
   email: String,
 })
 
+//Creation of object connectino for MongoDB Plant collecction
+const Plant = mongoose.model('Plant', {
+  user_email: String,
+  common_name: String,
+  scientific_name: String,
+  assigned_name: String,
+  plant_type: String,
+  description: String,
+  watering_frequency: String,
+  watering_amount: String
+})
+
 
 // Server method to handle user login
 app.post('/login/user', function (req, res) {
@@ -44,6 +56,54 @@ app.post('/login/user', function (req, res) {
       console.log('already have dem')
       res.status(200).json({ "message": "hello! you already exist!" })
     }
+  })
+
+});
+
+app.post('/greenhouse/plants', function(req, res) {
+  console.log('in add new plant')
+
+  new Plant(req.body).save().then((result) => {
+
+    console.log("Yay saved a plant!")
+    // Return a 201 indicating a new plant was created
+    res.status(201).json({"message":"a new plant was made woot!"})
+
+  }).catch((err) => {
+    console.log("darn something went wrong")
+    // Return a 500 if there was a problem inserting into the database
+    res.status(500).json({"error":err.message})
+  })
+
+});
+
+app.delete('/greenhouse/plants', function(req, res){
+  console.log("in delete plant func")
+
+  if(!req.body.id){
+    console.log("no plant id")
+    res.status(400).json({"message":"bad request - no plant id"})
+  }
+
+  Plant.findOne({ _id: req.body.id }).then(result => {
+    console.log("tryna find one")
+    if(!result){
+      console.log("no plant sad")
+      res.status(404).json({"message":"not found - plant does not exist"})
+    }
+
+    Plant.remove({'_id':req.body.id}).then((result) => {
+      console.log("in plant remove")
+      res.send(202).json({"message":"plant successfully deleted"})
+
+    }).catch((err) => {
+      console.log("failed to remove plant")
+      res.status(500).json({"error":err.message})
+    })
+
+  }).catch((err) => {
+    console.log("failed to find one")
+    res.status(500).json({"error":err.message})
   })
 
 });
